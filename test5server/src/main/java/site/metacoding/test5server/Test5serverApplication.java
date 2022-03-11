9ac038e6f75af6ef0cc50d62c9ac6ab18accb776
gih1214 <gih1214@hanmail.net>
2022-03-11 15:55:57 +0900

    자료형, 함수, dom 제어 공부

---
+++ test5server/src/main/java/site/metacoding/test5server/Test5serverApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.test5server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test5serverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test5serverApplication.class, args);
+	}
+
+}
---
+++ test5server/src/test/java/site/metacoding/test5server/Test5serverApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.test5server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test5serverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
