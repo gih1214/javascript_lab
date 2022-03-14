5cbbc314b8f602fc0ab623684464606fb6be2869
gih1214 <gih1214@hanmail.net>
2022-03-14 15:41:52 +0900

    @CrossOrigin으로 자바스크립트 요청 허용, JQuery Effects 공부

--- test5server/src/main/java/site/metacoding/test5server/NumberController.java
+++ test5server/src/main/java/site/metacoding/test5server/NumberController.java
@@ -10,11 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class NumberController {
 
-    @CrossOrigin // 자바스크립트 요청 허용!!
+    // 다른 도메인의 자바스크립트 요청만 금지
+    // 자바스크립트 요청 허용!!
+    @CrossOrigin // (methods = { RequestMethod.POST, RequestMethod.GET }, originPatterns =
+                 // "http://127.0.0.1/5500")
     @GetMapping("/number")
     public List<Integer> number() {
         List<Integer> list = Arrays.asList(1, 2, 3, 4);
+        System.out.println(list);
+        // View Resolver 작동 (@Controller) = 파일을 찾아주는 친구
+        // MessageConverter 작동 (@RestController) = JSON으로 변환해서 응답 {"data" : [1,2,3]}
+        return list; // [1,2,3,4]
+    }
 
-        return list;
+    @GetMapping("/user")
+    public User user() {
+        User u = new User(1, "ssar", "1234");
+        System.out.println(u);
+        return u;
     }
 }
